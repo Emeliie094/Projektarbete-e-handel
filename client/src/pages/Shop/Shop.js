d33f9f198a66bd59be1d5ab1a696b4c1dc8f6a6f
Emeliie094 <emeliie094@hotmail.com>
2021-02-15 21:49:03 +0100

    Add the start of a detail page

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -1,8 +1,12 @@
-const Shop = ()=>{
+import ProductCard from "../../components/ProductCard/ProductCard.js"
+import "../../components/ProductCard/ProductCard.css"
+
+const Shop = ({products})=>{
     return (
      <div>
          <h1>Shop</h1>
-        
+         <h1>**FILTER**</h1>
+        <ProductCard products={products}/>
          </div>
     );
  }
