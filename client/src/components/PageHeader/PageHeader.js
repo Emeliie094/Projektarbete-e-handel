f4b9ac0dc104bea9d49c9609d395e5b787746f28
Jenny Back <jenny@jennybacklin.se>
2021-02-15 14:14:00 +0100

    Add bootstrap menu, dropdown not working

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -2,13 +2,34 @@ import {NavLink} from "react-router-dom";
 
 const PageHeader = () => (
     <header>
-        <ul>
-            <li>
-                <NavLink  exact to = {"/"}>Home</NavLink>
-                <NavLink  exact to = {"/shop"}>Shop</NavLink>
-                <NavLink  exact to = {"/checkout"}>Checkout</NavLink>
-            </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <img className="navbar-brand" src="mystical2.12.png"></img>
+  <button className="navbar-toggler" type="button" dataToggle="collapse" dataTarget="#navbarSupportedContent" ariaControls="navbarSupportedContent" ariaExpanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+      <NavLink className="nav-link"  exact to = {"/"}>Home</NavLink>
+      </li>
+      <li className="nav-item">
+      <NavLink  className="nav-link" exact to = {"/shop"}>Shop</NavLink>
+      </li>
+      <li className="nav-item dropdown">
+      <NavLink className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" dataToggle="dropdown" ariaHaspopup="true" ariaExpanded="false"  exact to = {"/checkout"}>Checkout</NavLink>
+
+        <div className="dropdown-menu" ariaLabelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+        
     </header>
 )
 export default PageHeader;
