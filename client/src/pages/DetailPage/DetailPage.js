af7d854dd29bd63b9380166a39099e67f45e6368
Emeliie094 <emeliie094@hotmail.com>
2021-03-01 21:59:49 +0100

    add detail card to the detail page

--- client/src/pages/DetailPage/DetailPage.js
+++ client/src/pages/DetailPage/DetailPage.js
@@ -1,23 +1,25 @@
 import { useParams } from "react-router-dom";
-import "./DetailPage.css";
+// import "./DetailPage.css";
+import DetailCard from "./DetailCard/DetailCard"
+import { NavLink } from 'react-router-dom';
 
 const DetailsPage = ({ products }) => {
-  const { id } = useParams();
 
-  const product = products.find((product) => product.id == id);
-
+  
   return (
-    <div>
-      <div class="product-container">
-        <div class="img-container">
-          <img src={product.imageUrl} alt="{product.name}" />
-        </div>
-        <h3>{product.name}</h3>
-        <h4>{product.price} KR</h4>
-        <p>{product.description}</p>
-      </div>
-    </div>
+        <DetailCard products={products}/>
   );
 };
 
 export default DetailsPage;
+
+    // <div>
+    //   <div class="product-container">
+    //     <div class="img-container">
+    //       <img src={product.imageUrl} alt="{product.name}" />
+    //     </div>
+    //     <h3>{product.name}</h3>
+    //     <h4>{product.price} KR</h4>
+    //     <p>{product.description}</p>
+    //   </div>
+    // </div>
