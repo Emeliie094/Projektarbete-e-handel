613ce604ef3d5645624814156e27c991625426ce
Jenny Back <jenny@jennybacklin.se>
2021-03-06 10:37:27 +0100

    Add functions to open searchbar and cart responsive

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,33 +1,25 @@
-import {NavLink, Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {useState} from "react";
 import './PageHeaderStyle.css';
 
 const PageHeader = () => {
 
-    const [open,setOpen]=useState(false);
+    const [openMenu,setOpenMenu]=useState(false);
     const [openCart,setOpenCart]=useState(false);
-    // const [openSearch,setOpenSearch]=useState(false);
+    const [openSearch,setOpenSearch]=useState(false);
 
     const openMenuIcon = ()=>{
-        setOpen(!open);
-        console.log("menu open");
+        setOpenMenu(!openMenu)
 
     };
     const openCartIcon = ()=>{
-        setOpenCart(!open);
-        console.log("cart open");
-
-        const element = document.getElementsByClassName("shopping-nav");
-        // transform: open ? "translateX(0px)" : ""}
-
-
+        setOpenCart(!openCart);
 
     };
 
-    // const openSearchBar = ()=>{
-    //     setOpenSearch(!open);
-    //     console.log("search open");
-    // };
+    const openSearchBar = ()=>{
+        setOpenSearch(!openSearch);
+    };
 
 return (
     <header>
@@ -35,7 +27,7 @@ return (
             <div className="logo">
                 <img className="nav-logo"src="https://via.placeholder.com/50x50.png?text=Logo" alt="logo"/>
             </div>
-        <ul className="nav-links" style={{transform: open ? "translateX(0px)" : ""}}>
+        <ul className="nav-links" style={{transform: openMenu ? "translateX(0px)" : ""}}>
             <li>
                 <NavLink className="nav-link"  exact to = {"/"}>Home</NavLink>
             </li>
@@ -46,17 +38,22 @@ return (
                 <NavLink className="nav-link"  exact to = {"/checkout"}>Checkout</NavLink>
             </li>
         </ul>
-        <ul className="shopping-nav">
+        <div className="shopping-nav" style={{transform: openCart ? "translateX(0px)" : ""}}>
         
-            {/* <ul className="shopping-items">Shopping cart items</ul>
+            <ul className="shopping-items">Shopping cart items</ul>
             <li>Kristall 1, 450kr<button>Remove</button></li>
             <li>Kristall 2, 600kr<button>Remove</button></li>
             <li>Kristall 3, 900kr<button>Remove</button></li>
             <li>Sum:</li>
-            <button>Proceed to payment</button> */}
-        </ul>
+            <button>Proceed to payment</button>
+        </div>
+        <form className="search-bar" style={{transform: openSearch ? "translateX(0px)" : ""}}>
+            <input type="text" placeholder="search"></input>
+            <button>Search</button>
+        </form>
+       
         <i onClick={openMenuIcon} className="fas fa-bars burger"></i>
-        <i className="fas fa-search search"></i>
+        <i onClick={openSearchBar}className="fas fa-search search"></i>
         <i onClick={openCartIcon} className="fas fa-shopping-bag shop-bag"></i>
         </nav>
         
