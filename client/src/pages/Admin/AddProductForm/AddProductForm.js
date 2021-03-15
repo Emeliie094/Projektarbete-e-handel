796068f352a0ec4f12c3bafb81c552c98c8e6ee1
Emeliie094 <emeliie094@hotmail.com>
2021-03-15 11:26:52 +0100

    fix merge conflict

--- client/src/pages/Admin/AddProductForm/AddProductForm.js
+++ client/src/pages/Admin/AddProductForm/AddProductForm.js
@@ -154,23 +154,6 @@ const AddProductForm = () => {
                 <div className={styles.selectFields}>
 
                     <div>
-<<<<<<< HEAD
-                        <label for="zodiac">
-                        Zodiac
-                        <select className="select" multiple={true} onChange= {handleSelected} id="zodiac">
-                            <option className="select"value="Aries" >Aries| March 21-April 19</option>
-                            <option className="select"value="Taurus">Taurus | April 20 - May 20</option>
-                            <option className="select"value="Gemini">Gemini | May 21 - June 21</option>
-                            <option className="select"value="Cancer">Cancer | June 22 - July 22</option>
-                            <option className="select"value="Leo">Leo | July 23 - August 22</option>
-                            <option className="select"value="Virgo">Virgo | August 23 - September 22</option>
-                            <option className="select"value="Libra">Libra | September 23 - Oktober 23</option>
-                            <option className="select"value="Scorpius">Scorpius | Oktober 24 - November 21</option>
-                            <option className="select"value="Sagittarius">Sagittarius | November 22 - December 21</option>
-                            <option className="select"value="Capricornus">Capricornus | December 22 - Januari 19</option>
-                            <option className="select"value="Aquarius">Aquarius | January 20 - Februari 18</option>
-                            <option className="select"value="Pisces">Pisces| February 19 - March 20</option>
-=======
                         <label className={styles.zodiac}>Zodiac 
                         <select className={styles.selectZodiac} id="zodiac" multiple={true} onChange= {currentEventHandler} onClick={openZodiacSelection} style={{display: openZodiac ? "block" : ""}}>
                             <option className={styles.option} value="Aries">Aries| March 21-April 19</option>
@@ -185,7 +168,6 @@ const AddProductForm = () => {
                             <option className={styles.option}value="Capricornus">Capricornus | December 22 - Januari 19</option>
                             <option className={styles.option}value="Aquarius">Aquarius | January 20 - Februari 18</option>
                             <option className={styles.option}value="Pisces">Pisces| February 19 - March 20</option>
->>>>>>> admin
                         </select>
                         </label>    
                     </div>       
