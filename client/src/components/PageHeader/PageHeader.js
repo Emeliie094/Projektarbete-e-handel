42b6e32af1beff86e30886dbd05b262ae2442352
Emeliie094 <emeliie094@hotmail.com>
2021-03-30 13:48:41 +0200

    add image to cart

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,105 +1,131 @@
-import {NavLink, useHistory} from "react-router-dom";
-import {useState} from "react";
-import './PageHeaderStyle.css';
+import { NavLink, useHistory } from "react-router-dom";
+import { useState } from "react";
+import "./PageHeaderStyle.css";
 
-const PageHeader = ({cart, setCart}) => {
+const PageHeader = ({ cart, setCart }) => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [searchTerm, setSearchTerm]=useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event)=>{
-        event.preventDefault();
+    history.push({
+      pathname: "/search",
+      search: "?=${searchTerm}",
+    });
 
-        history.push({
+    console.log(searchTerm);
+  };
 
-            pathname: '/search',
-            search:'?=${searchTerm}'
-        });
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
 
-        console.log(searchTerm);
+  const openMenuIcon = () => {
+    setOpenMenu(!openMenu);
+  };
+  const openCartIcon = () => {
+    setOpenCart(!openCart);
+  };
 
-    };
+  const openSearchBar = () => {
+    setOpenSearch(!openSearch);
+  };
 
-    const [openMenu,setOpenMenu]=useState(false);
-    const [openCart,setOpenCart]=useState(false);
-    const [openSearch,setOpenSearch]=useState(false);
+  const removeFromCart = (productToRemove) => {
+    setCart(cart.filter((product) => product !== productToRemove));
+  };
 
-    const openMenuIcon = ()=>{
-        setOpenMenu(!openMenu)
+  function handleClick() {
+    history.push({
+      pathname: "/checkout",
+    });
+  }
 
-    };
-    const openCartIcon = ()=>{
-        setOpenCart(!openCart);
-
-    };
-
-    const openSearchBar = ()=>{
-        setOpenSearch(!openSearch);
-    }; 
-
-    const removeFromCart = (productToRemove) => {
-        setCart(cart.filter((product) => product !== productToRemove));
-      };
-
-      function handleClick() {
-        history.push({
-            pathname: '/checkout'
-        });
-      }
-
-return (
+  return (
     <header>
-        <nav>
-            <div className="logo">
-                <img className="nav-logo"src="https://via.placeholder.com/50x50.png?text=Logo" alt="logo"/>
-            </div>
-        <ul className="nav-links" style={{transform: openMenu ? "translateX(0px)" : ""}}>
-            <li>
-                <NavLink className="nav-link"  exact to = {"/"}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link"  exact to = {"/shop"}>Shop</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link"  exact to = {"/checkout"}>Checkout{` • ${cart.length} `}</NavLink>
-            </li>
-            
+      <nav>
+        <div className="logo">
+          <img
+            className="nav-logo"
+            src="https://via.placeholder.com/50x50.png?text=Logo"
+            alt="logo"
+          />
+        </div>
+        <ul
+          className="nav-links"
+          style={{ transform: openMenu ? "translateX(0px)" : "" }}
+        >
+          <li>
+            <NavLink className="nav-link" exact to={"/"}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" exact to={"/shop"}>
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" exact to={"/checkout"}>
+              Checkout{` • ${cart.length} `}
+            </NavLink>
+          </li>
         </ul>
-        <div className="shopping-nav" style={{transform: openCart ? "translateX(0px)" : ""}}>
-        
-            <ul className="shopping-items">Shopping cart items</ul>
+        <div
+          className="shopping-nav"
+          style={{ transform: openCart ? "translateX(0px)" : "" }}
+        >
+          <ul className="shopping-items">Shopping cart items</ul>
 
-
-            <hr></hr>
-            {cart.map((product) => (
-                <div>
-            <li className="li">{product.name}, {product.price}kr</li>
-            <button className="removeBtn" onClick={() => removeFromCart(product)}>Remove</button>
+          <hr></hr>
+          {cart.map((product) => (
+            <div>
+              <img src={product.imageUrl} />
+              <li className="li">
+                {" "}
+                {product.name}, {product.price}kr
+              </li>
+              <button
+                className="removeBtn"
+                onClick={() => removeFromCart(product)}
+              >
+                Remove
+              </button>
             </div>
-            ))}
-            <li>Sum:</li>
-            <button className="goToCartBtn" onClick={handleClick}>Proceed to payment</button>
+          ))}
+          <li>Sum:</li>
+          <button className="goToCartBtn" onClick={handleClick}>
+            Proceed to payment
+          </button>
         </div>
 
-        
-        <form onSubmit={handleSubmit}className="search-bar" style={{transform: openSearch ? "translateX(0px)" : ""}}>
-            <input 
+        <form
+          onSubmit={handleSubmit}
+          className="search-bar"
+          style={{ transform: openSearch ? "translateX(0px)" : "" }}
+        >
+          <input
             className="search-bar-field"
-            type="search" 
+            type="search"
             placeholder="search"
             value={searchTerm}
-            onChange={(e)=> setSearchTerm(e.target.value)} />
-            <button className="search-bar-btn"><i className="fas fa-search "></i></button>
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="search-bar-btn">
+            <i className="fas fa-search "></i>
+          </button>
         </form>
-       
-        <i onClick={openMenuIcon} className="fas fa-bars burger"></i>
-        <i onClick={openSearchBar}className="fas fa-search search"></i>
-        <i onClick={openCartIcon} className="fas fa-shopping-bag shop-bag">{` ${cart.length} `}</i>
-        </nav>
-        
-    </header>
-);
-}
-export default PageHeader;
 
+        <i onClick={openMenuIcon} className="fas fa-bars burger"></i>
+        <i onClick={openSearchBar} className="fas fa-search search"></i>
+        <i
+          onClick={openCartIcon}
+          className="fas fa-shopping-bag shop-bag"
+        >{` ${cart.length} `}</i>
+      </nav>
+    </header>
+  );
+};
+export default PageHeader;
